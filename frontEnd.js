67ea931e7a859d4d426a9a038dce34aa5f6719fa
Tristan K. Tarpley <Tristan@Sweet-Tea.local>
2018-03-09 17:38:20 -0600

    started building out alternate class designs

---
+++ frontEnd.js
@@ -0,0 +1,33 @@
+$('.oneSelect').click(function(){
+	$('#forClass > div:nth-child(1)').removeClass();
+	$('#forClass > div:nth-child(1)').addClass("one");
+	$(".headshot").attr("src","images/cheesin.jpg");	
+})
+
+$('.twoSelect').click(function(){
+	$('#forClass > div:nth-child(1)').removeClass();
+	$('#forClass > div:nth-child(1)').addClass("two");	
+	$(".headshot").attr("src","images/cheesin.jpg");	
+})
+
+$('.threeSelect').click(function(){
+	$('#forClass > div:nth-child(1)').removeClass();
+	$('#forClass > div:nth-child(1)').addClass("three");
+	$(".headshot").attr("src","images/clean.jpeg");	
+	$(".smile").css('display', 'none');
+	$('#cleanUp').toggleClass('inactive');
+})
+
+var flag = 1;
+function changeImage(){
+	if(flag == 1) {
+      $(".headshot").attr("src","images/muggin.jpg");
+      $(".smile").text('Click here to tell me a joke.');
+      flag = 0;
+    }
+    else if(flag == 0) {
+      $(".headshot").attr("src","images/cheesin.jpg");
+      $(".smile").text('Click again to make me mad.');
+      flag = 1;
+    }
+}
